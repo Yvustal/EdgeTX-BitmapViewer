361407d3149b8a8c1644b818642b8cb14edbcc38
Yvustal <hognelembanchemani@gmail.com>
2026-02-24 17:03:46 +0400

    Add files via upload

---
+++ BitmapViewer.lua
@@ -0,0 +1,30 @@
+local function init()
+end
+
+local function run(event)
+    lcd.clear()
+
+    -- Paths for the two 64x64 images
+    local imagePath1 = "/SCRIPTS/TOOLS/BITMAPVIEWER/bitmap1.bmp"
+    local imagePath2 = "/SCRIPTS/TOOLS/BITMAPVIEWER/bitmap2.bmp"
+
+    -- Load the first image
+    local img1 = io.open(imagePath1, "r")
+    if img1 then
+        io.close(img1)
+        -- Draw the first image at position (0, 0)
+        lcd.drawPixmap(0, 0, imagePath1)
+    end
+
+    -- Load the second image
+    local img2 = io.open(imagePath2, "r")
+    if img2 then
+        io.close(img2)
+        -- Draw the second image at position (64, 0)
+        lcd.drawPixmap(64, 0, imagePath2)
+    end
+
+    return 0
+end
+
+return { init=init, run=run }
